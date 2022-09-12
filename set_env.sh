9c1400dfffb3cea4fb9978f2896785ddcb5eaf97
ikram kumdin <bintkumdin@gmail.com>
2022-09-12 14:30:48 +0300

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Abatekumdin
+export POSTGRES_HOST=postgres.cfwn1s5kjixx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udasityprojectthreebucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
